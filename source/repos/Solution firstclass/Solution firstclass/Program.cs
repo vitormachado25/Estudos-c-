a5f58d711de24b91aed28a1f2773b276dd42c71c
Vitor Hugo Machado <victormarau2006@gmail.com>
2022-03-24 13:53:56 -0300

    commit inicial

---
+++ source/repos/Solution firstclass/Solution firstclass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_firstclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Torneira torneira = new Torneira();
+            Console.WriteLine("Digite uma opção:");
+            int escolha = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade:");
+            int Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite uma opção:");
+            int escolha = int.Parse(Console.ReadLine());
+
+            if (torneira.Abrir(escolha) == true)
+            {
+                torneira.Fechar();
+            }
+
+            Console.WriteLine("Valor a pagar" + Calcular(preco,Quantidade));
+            Console.WriteLine("A torneira está aberta?" + torneira.Estado);
+
+        }
+
+
+        static double Calcular(double valor, int quant)
+        {
+         return valor * quant;
+        }
+
+    }
+}
---
+++ source/repos/Solution firstclass/Solution firstclass/Torneira.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_firstclass
+{
+    class Torneira
+    {
+        public String Cor { get; set; }
+        public String Matérial { get; set; }
+        public int Tamanho { get; set; }
+        public int Altura { get; set; }
+        public bool Estado { get; set; }
+
+        public bool Abrir(int opcao)
+        { 
+            if(opcao == 1)
+            {
+                Estado = true;
+            }
+            return Estado;
+        }
+
+        public void Fechar()
+        {
+            Estado = false;
+
+        }
+
+        public void Jorrar()
+        {
+
+        }
+
+    }
+}
